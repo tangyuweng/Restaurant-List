f509c0cb9398f4b00d3604f298494c9b37ed3fa1
tangyuweng <tangyuw99@gmail.com>
2023-12-07 22:24:50 +0800

    feat: add route for updating restaurants

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
@@ -14,43 +15,41 @@ app.set('views', './views')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
 
 // 瀏覽全部所有餐廳，收尋餐廳
-app.get('/restaurants', (req, res) => {
-  const keyword = req.query.search?.trim()
-  if (keyword) {
-    const whereCondition = {
-      [db.Sequelize.Op.or]: [
-        {
-          name: {
-            [db.Sequelize.Op.like]: `%${keyword}%`
+app.get('/restaurants', async (req, res) => {
+  try {
+    const keyword = req.query.search?.trim()
+    let whereCondition = {}
+    if (keyword) {
+      whereCondition = {
+        [db.Sequelize.Op.or]: [
+          {
+            name: {
+              [db.Sequelize.Op.like]: `%${keyword}%`
+            }
+          },
+          {
+            category: {
+              [db.Sequelize.Op.like]: `%${keyword}%`
+            }
           }
-        },
-        {
-          category: {
-            [db.Sequelize.Op.like]: `%${keyword}%`
-          }
-        }
-      ]
+        ]
+      }
     }
-    return Restaurant.findAll({
+    const restaurants = await Restaurant.findAll({
       attributes: ['id', 'name', 'image', 'category', 'rating'],
       where: whereCondition,
       raw: true
     })
-      .then((restaurants) => res.render('index', { restaurants, keyword }))
-      .catch((err) => console.log(err))
-  } else {
-    return Restaurant.findAll({
-      attributes: ['id', 'name', 'image', 'category', 'rating'],
-      raw: true
-    })
-      .then((restaurants) => res.render('index', { restaurants }))
-      .catch((err) => console.log(err))
+    res.render('index', { restaurants, keyword })
+  } catch (err) {
+    console.log(err)
   }
 })
 
@@ -59,72 +58,73 @@ app.get('/restaurants/new', (req, res) => {
 })
 
 // 新增餐廳
-app.post('/restaurant', (req, res) => {
-  const {
-    name,
-    name_en,
-    category,
-    image,
-    location,
-    phone,
-    google_map,
-    rating,
-    description
-  } = req.body
-
-  return Restaurant.create({
-    name,
-    name_en,
-    category,
-    image,
-    location,
-    phone,
-    google_map,
-    rating,
-    description
-  })
-    .then(() => res.redirect('/restaurants'))
-    .catch((err) => console.log(err))
+app.post('/restaurant', async (req, res) => {
+  try {
+    await Restaurant.create(req.body)
+    res.redirect('/restaurants')
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 // 瀏覽指定餐廳
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id // id = string
-  return Restaurant.findByPk(id, {
-    attributes: [
-      'name',
-      'category',
-      'location',
-      'google_map',
-      'phone',
-      'description',
-      'image'
-    ],
-    raw: true
-  })
-    .then((restaurant) => res.render('show', { restaurant }))
-    .catch((err) => console.log(err))
+app.get('/restaurants/:id', async (req, res) => {
+  try {
+    const id = req.params.id // id = string
+    const restaurant = await Restaurant.findByPk(id, {
+      attributes: [
+        'name',
+        'name_en',
+        'category',
+        'image',
+        'location',
+        'phone',
+        'google_map',
+        'rating',
+        'description'
+      ],
+      raw: true
+    })
+    res.render('show', { restaurant })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
-// 編輯指定餐廳頁
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findByPk(id, {
-    attributes: [
-      'name',
-      'name_en',
-      'category',
-      'image',
-      'location',
-      'phone',
-      'google_map',
-      'rating',
-      'description'
-    ],
-    raw: true
-  })
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch((err) => console.log(err))
+// 編輯餐廳頁
+app.get('/restaurants/:id/edit', async (req, res) => {
+  try {
+    const id = req.params.id
+    const restaurant = await Restaurant.findByPk(id, {
+      attributes: [
+        'id',
+        'name',
+        'name_en',
+        'category',
+        'image',
+        'location',
+        'phone',
+        'google_map',
+        'rating',
+        'description'
+      ],
+      raw: true
+    })
+    res.render('edit', { restaurant })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+// 更新餐廳
+app.put('/restaurants/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    await Restaurant.update(req.body, { where: { id: id } })
+    res.redirect(`/restaurants/${id}`)
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 app.listen(port, () => {
