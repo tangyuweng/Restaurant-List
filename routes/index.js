c0fde32424981dcea9d539ffdb49eb7f97eb6c84
tangyuweng <tangyuw99@gmail.com>
2024-01-06 13:31:51 +0800

    feat: add user registration and login routes

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,23 @@ router.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
 
+router.get('/register', (req, res) => {
+  res.render('register', { noBody: true })
+})
+
+router.get('/login', (req, res) => {
+  res.render('login', { noBody: true })
+})
+
+router.post('/register', (req, res) => {
+  const body = req.body
+  res.send(
+    `name: ${body.name}, email: ${body.email}, password: ${body.password}, confirmPwd: ${body.confirmPwd}`
+  )
+})
+
+router.post('/login', (req, res) => {
+  res.send(req.body)
+})
+
 module.exports = router
